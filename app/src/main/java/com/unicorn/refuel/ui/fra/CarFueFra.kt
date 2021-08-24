27cbb9b0792b61aa4e15ae4ffd3facce43e222ab
ivotai <ivotai@163.com>
2021-08-24 21:49:40 +0800

    add scan code

--- app/src/main/java/com/unicorn/refuel/ui/fra/CarFueFra.kt
+++ app/src/main/java/com/unicorn/refuel/ui/fra/CarFueFra.kt
@@ -1,19 +1,26 @@
 package com.unicorn.refuel.ui.fra
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.list.listItems
+import com.blankj.utilcode.util.ConvertUtils
+import com.king.zxing.CameraScan
+import com.king.zxing.CaptureActivity
 import com.mikepenz.iconics.IconicsDrawable
 import com.mikepenz.iconics.typeface.library.fontawesome.FontAwesome
 import com.unicorn.refuel.app.safeClicks
 import com.unicorn.refuel.app.startAct
 import com.unicorn.refuel.app.toBeanList
+import com.unicorn.refuel.app.toast
 import com.unicorn.refuel.data.model.CarFuel
 import com.unicorn.refuel.data.model.base.EncryptionRequest
 import com.unicorn.refuel.data.model.base.PageRequest
@@ -23,34 +30,48 @@ import com.unicorn.refuel.databinding.FraCarFuelBinding
 import com.unicorn.refuel.ui.act.CarAct
 import com.unicorn.refuel.ui.adapter.CarFuelAdapter
 import com.unicorn.refuel.ui.fra.base.PageFra
-import com.unicorn.refuel.ui.pager.MainPagerAdapter
 import io.reactivex.rxjava3.core.Single
+
 
 class CarFueFra : PageFra<CarFuel>() {
 
     override fun initViews() = with(binding) {
         super.initViews()
-        with(extendedFloatingActionButton) {
+        with(efabCreate) {
             icon = IconicsDrawable(requireContext(), FontAwesome.Icon.faw_plus)
-            text = MainPagerAdapter.titles[0]
+            iconPadding = ConvertUtils.dp2px(8f)
+//            text = MainPagerAdapter.titles[0]
         }
     }
 
     @SuppressLint("CheckResult")
     override fun initBindings(): Unit = with(binding) {
         super.initBindings()
-        extendedFloatingActionButton.safeClicks().subscribe {
+        efabCreate.safeClicks().subscribe {
             MaterialDialog(requireContext()).show {
                 title(text = "选择加油车辆")
                 listItems(items = listOf("车辆扫码", "车辆列表选择")) { _, index, _ ->
                     if (index == 0) {
-                        // todo 扫码
+                        scanCode()
                     } else {
                         startAct(CarAct::class.java)
                     }
                 }
             }
         }
+    }
+
+    private fun scanCode() {
+        activityResultLauncher.launch(Intent(context, CaptureActivity::class.java))
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        data
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val result = CameraScan.parseScanResult(data)
+        result.toast()
+
     }
 
     override fun addItemDecoration() {
@@ -90,8 +111,17 @@ class CarFueFra : PageFra<CarFuel>() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FraCarFuelBinding.inflate(inflater, container, false)
+
+        activityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                val result = CameraScan.parseScanResult(it.data)
+                result.toast()
+            }
+
         return binding.root
     }
+
+    lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
 
     override fun onDestroyView() {
         super.onDestroyView()
